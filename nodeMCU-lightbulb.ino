8ce76650e9373a51e8d3f772f4c2ea2c1053582b
Abhishek Chakraborty <deadbeatfour@gmail.com>
2017-12-25 14:42:31 +0530

    Code dump with working Wifi login

---
+++ nodeMCU-lightbulb.ino
@@ -0,0 +1,26 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "Redmi";
+const char* password = "password";
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println();
+  Serial.print("connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
